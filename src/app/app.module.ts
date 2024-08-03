2a266b479bf194750944fc0d91a429d7a257457b
Apostolos Boutos <apboutos@gmail.com>
2024-08-03 16:53:23 +0300

    Login implementation
    -Imported LoginModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import {LoginModule} from "../component/login/login.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
